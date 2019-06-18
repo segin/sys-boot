6ee6e107572b8478fd8b3bfec89008f9bb6d3921
Kirn Gill II <segin2005@gmail.com>
2019-06-18 00:32:32 -0500

    Added hello-world fluff

--- payload.asm
+++ payload.asm
@@ -2,7 +2,21 @@
 
     org 7e00h
 _payload_start:
+    jmp msgend
 
+msg:    db "Hey, is this thing on?", 0
+msgend:
+    
+    mov bx, msg
+.loop:    
+    mov ah, 0eh
+
+    mov byte al, [bx]
+    int 10h
+    inc bx
+    cmp bx, msgend
+    jnz .loop
+    hlt
      
 times 0800h - ($ - $$)  db 0    ;Zerofill up to 2048 bytes
  
