495f0d13df711f62fcd9ef7b3110db4e1a8d5fa8
Kirn Gill II <segin2005@gmail.com>
2019-06-18 00:31:32 -0500

    Fixed up default payload and skipped worrying about es

--- boot.asm
+++ boot.asm
@@ -6,15 +6,14 @@ _bootblock_start:
 
 _start:
     ; set up "default" payload in case payload load fails
-    mov al, f4h ; halt opcode, so we don't just crash
-    mov [7e00], al
+    mov byte [7e00h], 0xf4 ; halt opcode, so we don't just crash
 
     ; load payload into RAM 
     mov ax, 0204h ; call 2, sectors = 4
     mov bx, 7e00h ; load immediately after this sector 
     mov cx, 0001h ; Disk 0x00
     mov dx, 0000h ; CHS 0/0/1
-    mov es, 0000h ; again, 0000:7e00
+;    xor es, es    ; again, 0000:7e00
     int 13h
 
     ; jump to payload
