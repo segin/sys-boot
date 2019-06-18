8e6be9b48b9cbecf2d8b1405bc987aa0a7ef2616
Kirn Gill II <segin2005@gmail.com>
2019-06-18 00:05:51 -0500

    Crude use of BIOS call to read payload added

--- boot.asm
+++ boot.asm
@@ -5,10 +5,17 @@ _bootblock_start:
     jmp short _start
 
 _start:
+    mov [7e00h], f4h ; move halt opcode in case read fails
+    mov ax, 0204h ; call 2, sectors = 4
+    mov bx, 7e00h ; load immediately after this sector 
+    mov cx, 0001h ; Disk 0x00
+    mov dx, 0000h ; CHS 0/0/1
+    mov es, 0000h ; again, 0000:7e00
+    int 13h
+    jmp short 7e00h
     hlt
-
      
 times 0200h - 2 - ($ - $$)  db 0    ;Zerofill up to 510 bytes
  
-    dw 0AA55h       ;Boot Sector signature
+    dw 0AA55h                       ;Boot Sector signature
 _bootblock_end:
