d486169b3df59ad411b702adf0e17f31f9a0162c
Kirn Gill II <segin2005@gmail.com>
2019-06-18 00:11:11 -0500

    Added default payload and code comments

--- boot.asm
+++ boot.asm
@@ -5,13 +5,19 @@ _bootblock_start:
     jmp short _start
 
 _start:
-    mov [7e00h], f4h ; move halt opcode in case read fails
+    ; set up "default" payload in case payload load fails
+    mov al, f4h ; halt opcode, so we don't just crash
+    mov [7e00], al
+
+    ; load payload into RAM 
     mov ax, 0204h ; call 2, sectors = 4
     mov bx, 7e00h ; load immediately after this sector 
     mov cx, 0001h ; Disk 0x00
     mov dx, 0000h ; CHS 0/0/1
     mov es, 0000h ; again, 0000:7e00
     int 13h
+
+    ; jump to payload
     jmp short 7e00h
     hlt
      
