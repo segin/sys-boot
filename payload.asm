4448261a4693b87cdbd6f594196e7d5217d98940
Kirn Gill II <segin2005@gmail.com>
2019-06-18 19:31:07 -0500

    Added more code comments

--- payload.asm
+++ payload.asm
@@ -7,17 +7,17 @@ _payload_start:
 msg:    db "Hey, is this thing on?", 0
 msgend:
     
-    mov cx, msg
+    mov cx, msg         ; move pointer to cx
 .loop:    
-    mov ah, 0eh
-    mov bx, cx
-    mov byte al, [bx]
-    mov bx, 0000h
-    int 10h
-    inc cx
-    cmp cx, msgend
-    jnz .loop
-    hlt
+    mov ah, 0eh         ; service call 0x0E, Write Character
+    mov bx, cx          ; Can't use cx as source address for
+    mov byte al, [bx]   ; move from memory
+    mov bx, 0000h       ; reset bx back to zero, the call uses bl
+    int 10h             ; call BIOS
+    inc cx              ; advance character pointer  
+    cmp cx, msgend      ; did we reach the end of the message?
+    jnz .loop           ; if not, jump back and display next byte
+    hlt                 ; stop the CPU.
      
 times 0800h - ($ - $$)  db 0    ;Zerofill up to 2048 bytes
  
